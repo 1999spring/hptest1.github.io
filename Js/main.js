ed23beb2a9ac6fa576e21d7f91cf06b7d0fd2cbd
1999spring <91887302+1999spring@users.noreply.github.com>
2023-10-19 21:07:42 +0900

    要らないコメントアウトとかを消した。

--- Js/main.js
+++ Js/main.js
@@ -1,83 +1,3 @@
-/* const includeHeader = new XMLHttpRequest();
-includeHeader.open("GET", "include/head.html", true);
-includeHeader.onreadystatechange = function () {
-  if (includeHeader.readyState === 4 && includeHeader.status === 200) {
-    const headerHTML = includeHeader.responseText;
-    const header = document.querySelector("#header");
-    header.insertAdjacentHTML("afterbegin", headerHTML);
-  }
-};
-includeHeader.send(); */
-
-/* const include = new XMLHttpRequest();
-include.open("GET", "include/"+"head.html", true);
-include.onreadystatechange = function () {
-  if (include.readyState === 4 && include.status === 200) {
-    const HTML = include.responseText;
-    const contents = document.querySelector("#header");
-    contents.insertAdjacentHTML("afterbegin", HTML);
-  }
-};
-include.send(); */
-
-const includer = (file_name,id) =>{
-  const include = new XMLHttpRequest();
-  include.open("GET", "include/"+file_name, true);
-  include.onreadystatechange = function () {
-    if (include.readyState === 4 && include.status === 200) {
-      const HTML = include.responseText;
-      const contents = document.querySelector("#"+id);
-      contents.insertAdjacentHTML("afterbegin", HTML);
-    }
-  };
-  include.send();
-};
-
-includer("head.html","head");
-includer("nav.html","nav");
-includer("article1.html","main-content");
-includer("side.html","side");
-includer("footer.html","footer")
-
-/* const includer1 = (file_name,id) =>{
-  const include = new XMLHttpRequest();
-  include.open("GET", "include/"+file_name, true);
-  console.log("include/"+file_name)
-  include.onreadystatechange = function () {
-    if (include.readyState === 4 && include.status === 200) {
-      const HTML = include.responseText;
-      const contents = document.querySelector("#"+id);
-      contents.insertAdjacentHTML("afterbegin", HTML);
-    }
-  };
-  include.send();
- 
-};
-includer1("footer.html","footer")
-window.addEventListener('load', function() {
-  // ページの読み込みが完了したらコンテンツを表示
-  this.setTimeout(()=>{
-    const test = document.querySelector(".footerFixed");
-  test.style.display = "block";
-  console.log(test.classList);
-  },1300)
-}); */
-/* window.onload = function (){
-  const test = document.querySelector(".footerFixed");
-  test.style.visibility = "visible";
-  console.log(test.classList);
-}; */
-/* const test = document.querySelector(".footerFixed");
-console.log(test.classList);
-test.style.visibility = "hidden" */
-
-/* window.onload = function (){
-  const test = document.querySelector(".footerFixed");
-  console.log(test.classList);
-  test.style.removeProperty("visibility")
-}; */
-
-
 // 画面の大きさがmin以下になったら要素にスタイルを設定する関数
 function checkViewportSize() {
   const min = 800; // 最小の幅（ピクセル単位）を設定
@@ -101,21 +21,6 @@ checkViewportSize();
 // ウィンドウのリサイズ時にも実行
 window.addEventListener("resize", checkViewportSize);
 
-/* // クラス名が "toc-005" の要素を取得
-const element = document.getElementsByClassName("toc-005");
-
-const elements = element[0].getElementsByTagName("a");
-
-
-elements[0].onmouseover = function () {
-  elements[0].style.color = "red";
-};
-
-elements[0].onmouseout = function () {
-  elements[0].style.color = "#333";
-};
- */
-
 
 const baseHtml = document.querySelector('.spreadsheets--item');
 const spreadsheets = document.querySelector('.spreadsheets');
@@ -136,10 +41,8 @@ async function loadData() {
     // フォーマットされた日付文字列を作成
     let formattedDate = year + '.' + month + '.' + day;
     //console.log(formattedDate); // "2023/10/2(月)"
-    copy.querySelector('.spreadsheets--name').textContent = entry.title;
-    copy.querySelector('.spreadsheets--name').href = entry.link;
-    copy.querySelector('.spreadsheets--age').textContent = "更新日："+formattedDate;
-    copy.querySelector('.spreadsheets--content').innerHTML = "内容："+entry.content;
+    copy.querySelector('.spreadsheets--name').textContent = 'タイトル：'+entry.title;
+    copy.querySelector('.link').href = entry.link;
     copy.querySelector('.spreadsheets--img').src = entry.image;
     spreadsheets.appendChild(copy);
   };
@@ -148,9 +51,38 @@ async function loadData() {
 
 loadData();
 
-const button = document.getElementById("myButton");
 
-// ボタンがクリックされたときの処理を設定
-button.addEventListener("click", function() {
-  window.location.reload()
-});
+const spreadsheets1 = document.querySelector('.js-base');
+console.log(spreadsheets1);
+
+if (spreadsheets1) {
+  spreadsheets1.parentNode.removeChild(spreadsheets1);
+}
+
+/* window.addEventListener('load', function() {
+  const textContainer = document.getElementById('text-container');
+  const text = textContainer.textContent;
+  const maxLines = 1; // 1行目の後に"..."を表示する
+  const lineHeight = parseFloat(getComputedStyle(textContainer).lineHeight);
+  const containerHeight = textContainer.clientHeight;
+  const lines = containerHeight / lineHeight;
+
+  if (lines > maxLines) {
+    const lineBreakIndex = text.indexOf('\n');
+    if (lineBreakIndex >= 0) {
+      const newText = text.slice(0, lineBreakIndex) + '...';
+      textContainer.textContent = newText;
+    }
+  }
+}); */
+
+/* window.addEventListener('load', function() {
+  const textContainer = document.getElementsByClassName('spreadsheets--name');
+  const computedStyle = getComputedStyle(textContainer[0]);
+  const lineHeight = parseFloat(computedStyle.lineHeight);
+  const containerHeight = textContainer[0].clientHeight;
+  const lines = containerHeight / lineHeight;
+  console.log(lineHeight);
+  console.log(containerHeight);
+  console.log('行数:', lines);
+}); */
